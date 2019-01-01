d492af909f33277db4f66088c10a1a9e9ecfe0b2
Jamie Ross <jamie.ross@marineenergy.ca>
2019-01-01 18:04:07 +0000

    changed file name

--- analysis1.jl
+++ analysis1.jl
@@ -1,4 +1,6 @@
 using DataFrames
 using CSV
+
 cd("/Users/jross/GitHub/energyflow")
-df = CSV.File("2018-07-01-2018-07-31.csv") |> DataFrame
+df = CSV.File("solar_July2018.csv") |> DataFrame
+StatsBase.median(df[8])
