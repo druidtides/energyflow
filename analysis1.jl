f55ffacc60b11846001726fa428b81e628005809
Jamie Ross <jamie.ross@marineenergy.ca>
2019-01-17 16:36:48 +0000

    added file compare but not done yet

--- analysis1.jl
+++ analysis1.jl
@@ -1,25 +1,33 @@
 using DataFrames
 using CSV
 using StatsBase
+using Dates
 #linux
 #cd("/home/jross/GitHub/energyflow")
 #mac
 cd("/Users/jross/GitHub/energyflow")
 #solar data is at 5 min intervals
-solar = CSV.File("solar_July2018.csv") |> DataFrame
+solar = CSV.File("solar_01July2018.csv") |> DataFrame
 #incomer data is at 1 min intervals
-incomer = CSV.File("Incomer-July2018.csv") |> DataFrame
+incomer = CSV.File("Incomer-01July2018.csv") |> DataFrame
 solar[8]
 StatsBase.median(solar[8])
 StatsBase.median(incomer[2,2])
 # generate combined file
 let
 rowIndex = 0
+inc_row = 1
 for pv_row in eachrow(solar)
     rowIndex = rowIndex + 1
-    #println("PV ",rowIndex," ",pv_row[1])
+
+    println("PV ",rowIndex," ",inc_row)
     t1= Dates.DateTime(pv_row[1], "yyyy/mm/dd HH:MM:SS")
-    t2= Dates.DateTime(incomer[rowIndex,1],"yyyy-mm-dd HH:MM:SS")
-    println(t1,t2,t2>t1)
+    t2= Dates.DateTime(incomer[inc_row,1],"yyyy-mm-dd HH:MM:SS")
+    println(t1," ",t2,t2<t1)
+    while t2 < t1
+        inc_row = inc_row + 1
+        t2= Dates.DateTime(incomer[inc_row,1],"yyyy-mm-dd HH:MM:SS")
+    end
+    println(t1,t2,t2<t1, inc_row)
 end
 end
